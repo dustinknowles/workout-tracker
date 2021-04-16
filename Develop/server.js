4f62928cbe1cc1ebbd8b4c05cad1d9d0639d8b18
dustin.knowles <dustin.knowles22@gmail.com>
2021-04-15 17:36:05 -0700

    adding server and database connections

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use(morgan("tiny"))
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+require("./routes/api")(app);
+require("./routes/html")(app);
+
+app.listen(PORT, function () {
+    console.log("==> Listening on port %s!", PORT, PORT)
+});
